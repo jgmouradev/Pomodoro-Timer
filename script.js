6a49f2bca4d1c9268333fbc06a200e2567b3f7b3
João Gabriel Gonçalves <90291555+jgmouradev@users.noreply.github.com>
2023-03-23 11:48:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const botaoIniciar = document.getElementById('#botaoIniciar')
+const descansoCurto = document.getElementById('#curto')
+const descansoLongo = document.getElementById('#longo')
+
+const pomodoro = 1500 // 25 minutos em segundos
+const curto = 300 // 5 minutos em segundos
+const longo = 900 // 15 minutos em segundos
+
+
