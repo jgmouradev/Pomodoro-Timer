c9dfef0325cd1fe30fa7ef414706a0750c745440
João Gabriel Gonçalves <90291555+jgmouradev@users.noreply.github.com>
2023-04-06 11:40:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-const botaoIniciar = document.getElementById('#botaoIniciar')
-const descansoCurto = document.getElementById('#curto')
-const descansoLongo = document.getElementById('#longo')
+const timerDisplay = document.querySelector('#timer');
+    const startButton = document.querySelector('#start_stop');
+    const resetButton = document.querySelector('#reset');
+    const progressBar = document.querySelector('#progress-bar-fill');
+    let timeLeft = 1500; // 25 minutes in seconds
+    let timerId;
+    let isTimerRunning = false;
 
-const pomodoro = 1500 // 25 minutos em segundos
-const curto = 300 // 5 minutos em segundos
-const longo = 900 // 15 minutos em segundos
+    function formatTimeLeft(time) {
+      const minutes = Math.floor(time / 60);
+      const seconds = time % 60;
+      return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
 
+    function updateTimer() {
+      timeLeft--;
+      timerDisplay.innerText = formatTimeLeft(timeLeft);
+      progressBar.style.width = `${((1500 - timeLeft) / 1500) * 100}%`;
+      if (timeLeft <= 0) {
+        clearInterval(timerId);
+        timerDisplay.innerText = '25:00';
+        isTimerRunning = false;
+      }
+    }
 
+    function startTimer() {
+      if (!isTimerRunning) {
+        timerId = setInterval(updateTimer, 1000);
+        isTimerRunning = true;
+      } 
+    }
+
+    function resetTimer() {
+      clearInterval(timerId);
+      timeLeft = 1500;
+      timerDisplay.innerText = '25:00';
+      progressBar.style.width = '0%';
+      isTimerRunning = false;
+    }
+
+    startButton.addEventListener('click', startTimer);
+    resetButton.addEventListener('click', resetTimer);
